a20698b26f8f8914431c0fc82a0be49e099cfdce
henry <hentimes@gmail.com>
2021-06-20 19:42:26 -0400

    Dibujar triangulo invertido

--- tarea3/tarea3.py
+++ tarea3/tarea3.py
@@ -2,3 +2,20 @@
 
 # Crear 4 triangulos rectangulos con altura/limite definitos
 # por input ingresado por usuario.
+
+l = int(input("Ingrese un valor: "))
+
+"""
+Manera poco efectiva de hacerlo.
+
+for i in range(0, l):
+  print ("*", end="")
+print ("")
+for j in range(l -1):
+  print ("*", end="")
+print ("")
+"""
+
+for i in range(l):
+  i = (l - i)
+  print ("*" * i)
