0e712b1e32a237b918ca00cdbcf5366f567e7ad0
henry <hentimes@gmail.com>
2021-06-27 15:58:41 -0400

    Se atendieron la mayoria de las sugerencias del code review

--- tarea3/tarea3.py
+++ tarea3/tarea3.py
@@ -3,40 +3,28 @@
 # Crear 4 triangulos rectangulos con altura/limite definitos
 # por input ingresado por usuario.
 
-l = int(input("Ingrese un valor: "))
-n = 0
-"""
-Manera poco efectiva de hacerlo.
-
-for i in range(0, l):
-  print ("*", end="")
-print ("")
-for j in range(l -1):
-  print ("*", end="")
-print ("")
-"""
+N = int(input("Ingrese un valor: "))
 
 # Imprime el primer triagunlo creciente
-for i in range(1, l+1, 1):
+for i in range(1, N+1, 1):
   print ("*" * i)
 print ("")
 
 # Imprime segundo triangulo decreciente
-for i in range(l, 0, -1):
+for i in range(N, 0, -1):
   print ("*" * i)
 print ("")
 
 # Imprime tercer triangulo invertido creciente
-for i in range(l):
-  espacios = (l - i - 1)
-#  j = (j + 1)
+for i in range(N):
+  espacios = (N - i - 1)
   print (" " * espacios, end="")
   print ("*" * (i+1))
 print ("")
 
 # Imprime cuarto triangulo invertido decreciente
-for i in range(l): # i = [0, 9]
+for i in range(N): # i = [0, 9]
   espacios = i  # [0, 9)
-  asteriscos = l - i # [10, 1]
+  asteriscos = N - i # [10, 1]
   print (" " * espacios + "*" * asteriscos)
   
